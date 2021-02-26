81186dbea2d82bd179041a8f609da5cd1d91d507
jawanggit <jawang.um@gmail.com>
2021-02-26 11:22:04 -0800

    enabled server

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#server = app.server
+server = app.server
 
 app.layout = html.Div([ 
      dcc.Tabs(id='tabs-example', value='tab-crime', children=[
